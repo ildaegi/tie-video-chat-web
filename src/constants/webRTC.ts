bf5f8cd566b17591d08d5069f60c1d9197e0ccba
outsung <outsung@users.noreply.github.com>
2022-02-27 08:24:14 +0000

    deploy: 81a9df9cd2a25d5f42d984594479fb2ff2ae7cb9

--- src/constants/webRTC.ts
+++
@@ -1,3 +0,0 @@
-export const RTC_PEER_CONNECTION_CONFIG = {
-  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-};
--- src/types/domain/user/user.types.ts
+++
@@ -1,1 +0,0 @@
-export type MeetingUser = { id: string; email: string; stream: MediaStream };
